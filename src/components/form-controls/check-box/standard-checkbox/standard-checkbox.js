ef1b85fe4c954bdc5e301ec75ffa007cc7e460e0
unknown <naidu.gorantla@cognitonic.com>
2024-10-24 12:43:53 +0530

    update

---
+++ src/components/form-controls/check-box/standard-checkbox/standard-checkbox.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const StandardCheckbox = () => {
+  // State to manage the checkbox
+  const [isChecked, setIsChecked] = useState(false);
+
+  // Handler for checkbox change
+  const handleCheckboxChange = () => {
+    setIsChecked(prevState => !prevState);
+  };
+
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        check the box if you are not
+      </label>
+      <p>{isChecked ? 'Checkbox is checked' : 'Checkbox is unchecked'}</p>
+    </div>
+  );
+};
+
+export default StandardCheckbox;
