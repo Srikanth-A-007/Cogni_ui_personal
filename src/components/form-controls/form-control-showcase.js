5c81187f630dbd1b2a4ea3db07e7868dc04ee0bf
Srikanth-A-007 <srikanthanguluri@gmail.com>
2024-10-24 14:43:42 +0530

    updated link button code

--- src/components/form-controls/form-control-showcase.js
+++ src/components/form-controls/form-control-showcase.js
@@ -1,4 +1,3 @@
-// C:\Users\Srikanth Anguluri\cogni_reacttest\src\components\form-controls\form-control-showcase.js
 import React from 'react';
 import LinkButton from './button/link/link-button'; // Update the path to link-button.js
 
@@ -7,8 +6,7 @@ const FormControlShowcase = () => {
     <div>
       {/* Example Button styled as a link with onClick behavior */}
       <LinkButton 
-        label="Click me" 
-        onClick={() => window.open('https://www.google.com', '_blank')} // Open Google homepage in a new tab
+        label="Go to Google"  // Provide the label for the button
       />
     </div>
   );
