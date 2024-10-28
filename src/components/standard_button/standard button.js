26710f3417cc6cda2b6593a9bafdb6837505f805
Srikanth-A-007 <srikanthanguluri@gmail.com>
2024-10-28 14:46:03 +0530

    Updates

--- src/components/standard_button/standard button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../components.css'; // Adjust for components.css
-import './standard_button.css'; // Correct for the new folder structure
-
-function StandardButton() {
-  return <button>Click Me</button>;
-}
-
-export default StandardButton;
